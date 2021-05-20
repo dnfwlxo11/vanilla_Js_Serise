027ed80928b4cb0f07d8f3dc2e593772ca7e3990
daein <dnfwlxo11@naver.com>
2021-05-20 16:23:42 +0900

    add: weather func and css

---
+++ ToDo_list/weather.js
@@ -0,0 +1,59 @@
+const COORDS = 'coords';
+const API_KEY = '03c89758314f4f37ee120fad3b13cca8';
+
+const $weather_span = document.createElement('span');
+$weather_span.setAttribute('class', 'js-date');
+
+function getWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+        .then(res => {
+            return res.json();
+        })
+        .then(res => {
+            const temp = res.main.temp;
+            const place = res.name;
+            $weather_span.innerText = `${temp}℃ / ${place}`;
+        })
+}
+
+function handleGeoSuccess(position) {
+    console.log(position);
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    const coordsObj = {
+        lat,
+        lon
+    };
+
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+    getWeather(lat, lon);
+}
+
+function handleGeoError() {
+    console.log('Can\'t load geo location');
+}
+
+function getCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem('COORDS')
+
+    if (loadedCoords === null) {
+        getCoords();
+    } else {
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
+    }
+}
+
+function init() {
+    const body = document.querySelector('body')
+    body.appendChild($weather_span);
+
+    loadCoords();
+}
+
+init();
