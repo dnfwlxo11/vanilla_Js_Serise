09d8bd269c81152b84ffb7a3e8edadc87c70a646
daein <dnfwlxo11@naver.com>
2021-05-20 14:05:44 +0900

    add: todo add, remove func

--- ToDo_list/todo.js
+++ ToDo_list/todo.js
@@ -7,11 +7,29 @@ $todo_form.setAttribute('class', 'js-todo-form');
 $todo_input.setAttribute('class', 'js-todo-input');
 $todo_input.setAttribute('type', 'text');
 $todo_input.setAttribute('placeholder', '메모를 입력하세요');
-$todo_button.setAttribute('class', 'js-todo-button');
-$todo_button.setAttribute('type', 'submit');
+// $todo_button.setAttribute('class', 'js-todo-button');
+// $todo_button.setAttribute('type', 'submit');
 $todo_ul.setAttribute('class', 'js-todo-ul')
 
 const TODO = 'todos';
+let Todos = [];
+
+function deleteTodo(e) {
+    const target_li = e.target.parentNode;
+    $todo_ul.removeChild(target_li);
+
+    const removed_Todos = Todos.filter((Todos) => {
+        return Todos.id !== parseInt(target_li.id)
+    });
+
+    console.log(Todos, removed_Todos)
+    Todos = removed_Todos;
+    saveTodos();
+}
+
+function saveTodos() {
+    localStorage.setItem(TODO, JSON.stringify(Todos));
+}
 
 function paintTodo(text) {
     const $li = document.createElement('li')
@@ -19,12 +37,25 @@ function paintTodo(text) {
 
     const $deleteBtn = document.createElement('button');
     $deleteBtn.innerText = "X";
+    $deleteBtn.addEventListener('click', deleteTodo);
 
     const $span = document.createElement('span');
     $span.innerText = text;
 
+    const newId = Todos.length + 1;
+
+    $li.id = newId;
+
     $li.appendChild($span);
     $li.appendChild($deleteBtn);
+
+    const todoObj = {
+        text: text,
+        id: newId
+    };
+
+    Todos.push(todoObj);
+    saveTodos();
 }
 
 function handleSubmit(e) {
@@ -36,16 +67,23 @@ function handleSubmit(e) {
 }
 
 function loadTodo() {
-    const todos = localStorage.getItem(TODO);
+    const loadTodos = localStorage.getItem(TODO);
 
-    $todo_form.addEventListener('submit', handleSubmit);
+    if (loadTodos !== null) {
+        const parseJson = JSON.parse(loadTodos);
+
+        parseJson.forEach((toDo) => {
+            paintTodo(toDo.text)
+        })
+    }
 }
 
 function init() {
     const body = document.querySelector('body');
     body.appendChild($todo_form);
     $todo_form.appendChild($todo_input);
-    $todo_form.appendChild($todo_button);
+    $todo_form.addEventListener('submit', handleSubmit);
+    // $todo_form.appendChild($todo_button);
     body.appendChild($todo_ul);
 
     $todo_button.innerText = '추가';
