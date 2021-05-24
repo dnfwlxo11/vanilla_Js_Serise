1af138513116725b2a45f67b560e2910cf57b4a7
daein <dnfwlxo11@naver.com>
2021-05-24 13:42:18 +0900

    add: timer.js

---
+++ Typing_traning_page/timer.js
@@ -0,0 +1,23 @@
+const $timer_div = document.createElement('div');
+
+$timer_div.setAttribute('class', 'timer-div');
+
+function setTimer() {
+    let getSecond = $timer_div.innerText;
+    $timer_div.innerText = parseInt(getSecond, 10) + 1;
+}
+
+function updateTimer() {
+    setInterval(setTimer, 1000);
+}
+
+function init() {
+    const body = document.querySelector('body');
+    $timer_div.innerText = 0;
+
+    body.appendChild($timer_div);
+
+    updateTimer();
+}
+
+init();
