89579a09be82f66200de5fff913057b94432243d
daein <dnfwlxo11@naver.com>
2021-05-25 09:18:57 +0900

    add: draw line to canvas

--- catchMind/canvas.js
+++ catchMind/canvas.js
@@ -1,9 +1,20 @@
-const $canvas = document.createElement('canvas');
+const $canvas = document.createElement('canvas'),
+    ctx = $canvas.getContext('2d');
 
 $canvas.setAttribute('id', 'jsCanvas');
 $canvas.setAttribute('class', 'jsCanvas');
 
 let painting = false;
+
+$canvas.width = 700;
+$canvas.height = 700;
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
+
+function startPainting() {
+    painting = true;
+}
 
 function stopPainting() {
     painting = false;
@@ -12,21 +23,21 @@ function stopPainting() {
 function onMouseMove(e) {
     const x_pos = e.offsetX;
     const y_pos = e.offsetY;
-}
 
-function onMouseDown(e) {
-    painting = true;
-}
-
-function onMouseUp(e) {
-    painting = false;
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x_pos, y_pos);
+    } else {
+        ctx.lineTo(x_pos, y_pos);
+        ctx.stroke();
+    }
 }
 
 function canvasEvent() {
     if ($canvas) {
       $canvas.addEventListener('mousemove', onMouseMove);
-      $canvas.addEventListener('mousedown', onMouseDown);
-      $canvas.addEventListener('mouseup', onMouseUp);
+      $canvas.addEventListener('mousedown', startPainting);
+      $canvas.addEventListener('mouseup', stopPainting);
       $canvas.addEventListener('mouseleave', stopPainting);
     }
 }
